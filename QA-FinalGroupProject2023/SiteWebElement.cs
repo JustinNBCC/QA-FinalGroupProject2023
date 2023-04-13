2228b41963a1f4ee2998e9b7605a40fbebecebc1
Justin Rioux <122111862+3Rioux@users.noreply.github.com>
2023-04-13 11:16:47 -0300

    added the SiteWebElement code

--- QA-FinalGroupProject2023/SiteWebElement.cs
+++ QA-FinalGroupProject2023/SiteWebElement.cs
@@ -8,5 +8,35 @@ namespace QA_FinalGroupProject2023
 {
     class SiteWebElement
     {
+        public static IWebElement Username(IWebDriver driver)
+        {
+            IWebElement txtLoginUsername = driver.FindElement(By.Id("username"));
+            return txtLoginUsername;
+        }
+        
+        public static IWebElement Password(IWebDriver driver)
+        {
+            IWebElement txtLoginPassword = driver.FindElement(By.Id("password"));
+            return txtLoginPassword;
+        }
+        
+        public static IWebElement LoginButton(IWebDriver driver)
+        {
+            IWebElement btnLogin = driver.FindElement(By.Id("button"));
+            return btnLogin;
+        }
+        
+        public static IWebElement EmailLink(IWebDriver driver)
+        {
+            IWebElement lnkEmail = driver.FindElement(By.CssSelector("p > a[href*='mailto:']"));
+            return lnkEmail;
+        }
+        
+        public static IWebElement Logo(IWebDriver driver)
+        {
+            IWebElement imgLogo = driver.FindElement(By.CssSelector("a.navbar-brand > img.logo"));
+            return imgLogo;
+        }
+        
     }
 }
